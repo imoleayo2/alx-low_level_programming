a8a8ccc45bad7349d1c201a8c4b9479ed3614015
imoleayo2 <outstandingclassic@gmail.com>
2023-02-18 13:17:16 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,16 @@
 int main(void)
 {
 	int i;
-	char j;
 
 	for (i = 0 ; i < 10 ; i++)
+	{
 		putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; j++)
-		putchar(j);
+		if (i != '0')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 	putchar('\n');
 	return (0);
 
