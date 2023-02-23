d8c6bb08f0890ccd500c7bea2a56df6c565b8e11
imoleayo2 <outstandingclassic@gmail.com>
2023-02-22 18:35:45 -0800

    lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - function to print alphabet
+ *
+ * 0c: parameter to be printed
+ *
+ * Return: return 1 if it is lower case
+ * and 0 otherwise
+ */
+
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+
+}
