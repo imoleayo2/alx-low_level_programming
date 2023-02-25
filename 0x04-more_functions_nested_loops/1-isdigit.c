c5fde849467dc8df3476c24a2eed0b4c0d2f046f
imoleayo2 <outstandingclassic@gmail.com>
2023-02-24 19:14:51 -0800

    cap

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - a function that checks for a digit
+ *
+ * @c: parameter
+ * Return: int
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
+}
